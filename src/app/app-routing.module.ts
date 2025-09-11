f4496b05180e25c22ff675e11dce08df7f2a9ef6
EduardGuilherme <eduguilherme48@gmail.com>
2025-09-11 19:21:19 -0300

    feat/update-appModules-appRouting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UsuariosComponent } from './components/usuarios/usuarios.component';
+import { TarefaComponent } from './components/tarefa/tarefa.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'usuarios', component: UsuariosComponent },
+  { path: 'tarefas', component: TarefaComponent },
+  { path: '', redirectTo: 'usuarios', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
