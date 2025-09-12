01325de3aea601d1ad79d6e870b5977b87c43060
EduardGuilherme <eduguilherme48@gmail.com>
2025-09-11 21:52:06 -0300

    feat/updateUsuarioComponent

--- src/app/components/usuarios/usuarios.component.ts
+++ src/app/components/usuarios/usuarios.component.ts
@@ -1,5 +1,6 @@
 import { Component,OnInit } from '@angular/core';
 import { UsuarioService } from '../../services/usuario.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-usuarios',
@@ -7,13 +8,31 @@ import { UsuarioService } from '../../services/usuario.service';
   styleUrls: ['./usuarios.component.css']
 })
 export class UsuariosComponent implements OnInit {
-  usuarios: any[] = [];
+ usuarios: any[] = [];
+  usuarioForm: FormGroup;
 
-  constructor(private usuarioService: UsuarioService) {}
+  constructor(private usuarioService: UsuarioService, private fb: FormBuilder) {
+    this.usuarioForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      senha: ['', Validators.required]
+    });
+  }
 
   ngOnInit(): void {
-    this.usuarioService.getUsuarios().subscribe(data => {
-      this.usuarios = data;
-    });
+    this.loadUsuarios();
+  }
+
+  loadUsuarios(): void {
+    this.usuarioService.getUsuarios().subscribe(data => this.usuarios = data);
+  }
+
+  submitUsuario(): void {
+    if (this.usuarioForm.valid) {
+      this.usuarioService.addUsuario(this.usuarioForm.value).subscribe(() => {
+        this.usuarioForm.reset();
+        this.loadUsuarios();
+      });
+    }
   }
 }
