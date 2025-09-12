01e77eacb8618652856a595c6acd2011d29b88a8
EduardGuilherme <eduguilherme48@gmail.com>
2025-09-11 21:52:50 -0300

    feat/add-edittarefa

--- src/app/services/tarefa.service.ts
+++ src/app/services/tarefa.service.ts
@@ -18,6 +18,10 @@ export class TarefaService {
     return this.http.post<any>(this.apiUrl, tarefa);
   }
 
+  updateTarefa(id: number, tarefa: any): Observable<any> {
+  return this.http.put(`${this.apiUrl}/${id}`, tarefa);
+}
+
   deleteTarefa(id: number): Observable<any> {
     return this.http.delete<any>(`${this.apiUrl}?id=${id}`);
   }
